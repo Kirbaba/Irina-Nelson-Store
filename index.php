bf41976f0a5e82324996ffe4ceb778d364a777ec
Vladislav <v.kirbaba@gmail.com>
2015-10-03 10:24:33 +0300

    Start

---
+++ index.php
@@ -0,0 +1,44 @@
+﻿<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <link rel="icon" href="/wp-content/uploads/2015/03/657068.ico" type="image/x-icon" />
+    <link rel="shortcut icon" href="/wp-content/uploads/2015/03/657068.ico" type="image/x-icon" />
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAaOWKyamSxMTXclSDFmJ2N4Am20PCTD6I&sensor=FALSE">
+    </script>
+    <title><?php bloginfo('name'); ?> <?php wp_title(); ?></title>
+    <?php wp_head(); ?>
+</head>
+<body>
+	<header class="header">
+		<nav class="navbar navbar-default navigation" role="navigation">
+		  <div class="container">
+		    <!-- Brand and toggle get grouped for better mobile display -->
+		    <div class="navbar-header">
+		      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+		            <span class="sr-only">Toggle navigation</span>
+		            <span class="icon-bar"></span>
+		            <span class="icon-bar"></span>
+		            <span class="icon-bar"></span>
+		          </button>
+		      <a class="navbar-brand" href="#">Logo</a>
+		    </div>
+		    <!-- Collect the nav links, forms, and other content for toggling -->
+		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">		      	     
+		      <ul class="nav navbar-nav navbar-right navigation__list">		      
+		        <li><a href="#">Главная</a></li>
+		        <li><a href="#">Акции</a></li>
+		        <li><a href="#">Статьи</a></li>
+		        <li><a href="#">Отзывы</a></li>
+		        <li><a href="#">Подписка</a></li>  		       
+		      </ul>
+		    </div><!-- /.navbar-collapse -->
+		  </div><!-- /.container-fluid -->
+		</nav>
+	</header>
+    
+<?php wp_footer(); ?>
+</body>
+</html>
