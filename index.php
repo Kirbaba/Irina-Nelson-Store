e57d180dae97b17666c136c548a5d7568fa80187
Vladislav <v.kirbaba@gmail.com>
2015-10-03 11:52:39 +0300

    header

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 </head>
 <body>
 	<header class="header">
-		<nav class="navbar navbar-default navigation" role="navigation">
+		<nav class="navbar navbar-default navbar-fixed-top navigation" role="navigation">
 		  <div class="container">
 		    <!-- Brand and toggle get grouped for better mobile display -->
 		    <div class="navbar-header">
@@ -22,12 +22,11 @@
 		            <span class="icon-bar"></span>
 		            <span class="icon-bar"></span>
 		            <span class="icon-bar"></span>
-		          </button>
-		      <a class="navbar-brand" href="#">Logo</a>
+		          </button>		      
 		    </div>
 		    <!-- Collect the nav links, forms, and other content for toggling -->
 		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">		      	     
-		      <ul class="nav navbar-nav navbar-right navigation__list">		      
+		      <ul class="nav navbar-nav navbar-center navigation__list">		      
 		        <li><a href="#">Главная</a></li>
 		        <li><a href="#">Акции</a></li>
 		        <li><a href="#">Статьи</a></li>
@@ -37,6 +36,22 @@
 		    </div><!-- /.navbar-collapse -->
 		  </div><!-- /.container-fluid -->
 		</nav>
+		<div class="container">
+			<h1 class="block_title">Магазин экопродуктов Ирины Нельсон</h1>
+			<h4><span>Элитный Иван-чай</span> по цене обычного</h4>
+			<div class="row">
+				<div class="col-lg-5 col-md-5 col-sm-12 col-xs-12 header__slogan">
+					<h4>Позвольте повысить качество вашей жизни</h4>	
+				</div>
+				<div class="col-lg-2 col-md-2 col-sm-12 col-xs-12 header__logo">
+					<img src="<?php bloginfo('template_directory'); ?>/img/logo.png" alt="">
+				</div>
+				<div class="col-lg-5 col-md-5 col-sm-12 col-xs-12 header__phone">
+					<h2 class="number">8 800 003 99 22</h2>
+					<p>Свяжитесь с нами</p>
+				</div>
+			</div>
+		</div>
 	</header>
     
 <?php wp_footer(); ?>
